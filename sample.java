f97a6a4b5bb6f2e45586f93a9e78cea1cf37f582
kasidevops <54234333+kasidevops@users.noreply.github.com>
2022-08-29 21:07:26 +0530

    Create sample.java
    Adding Numbers in java file

---
+++ sample.java
@@ -0,0 +1,20 @@
+public class SumOfNaturalNumber4
+{
+//method that returns the sum of n natural numbers 
+static int naturalNumberSum(int n) 
+{ 
+int sum = 0; 
+//executes until the condition becomes false
+for (int i = 1; i <= n; i++)  
+//adding the value of i to the sum variable
+sum = sum + i; 
+return sum; 
+} 
+//main method
+public static void main(String args[]) 
+{ 
+int n = 50; 
+//calling method and prints the sum
+System.out.println("Sum of Natural Numbers is: "+naturalNumberSum(n));
+}  
+} 
